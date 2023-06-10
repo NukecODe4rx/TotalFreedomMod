77755f62b2decf8cde66cb045b7f72fc29899c39
Luna <lunahatesgogle@gmail.com>
2023-06-10 14:15:47 -0300

    Async /tprandom

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_tprandom.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_tprandom.java
@@ -2,8 +2,7 @@ package me.totalfreedom.totalfreedommod.command;
 
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FUtil;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,12 +14,21 @@ public class Command_tprandom extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        int x = FUtil.randomInteger(-50000, 50000);
-        int z = FUtil.randomInteger(-50000, 50000);
-        int y = playerSender.getWorld().getHighestBlockYAt(x, z);
-        Location location = new Location(playerSender.getLocation().getWorld(), x, y, z);
-        playerSender.teleportAsync(location);
-        msg("Poof!", ChatColor.GREEN);
+        final int x = FUtil.randomInteger(-50000, 50000);
+        final int z = FUtil.randomInteger(-50000, 50000);
+        final World world = playerSender.getWorld();
+        Location location = new Location(playerSender.getLocation().getWorld(), x, 0, z);
+
+        server.getScheduler().runTaskAsynchronously(plugin, () -> world.getChunkAtAsync(location).whenCompleteAsync((chunk, throwable) -> {
+            if (throwable != null) {
+                throwable.printStackTrace();
+                return;
+            }
+
+            location.setY(playerSender.getWorld().getHighestBlockYAt(x, z));
+            playerSender.teleportAsync(location);
+            msg("Poof!", ChatColor.GREEN);
+        }));
         return true;
     }
 }
